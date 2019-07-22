100e2a3fe5357121969e9d082b5d6714002fd1b3
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-22 19:10:44 -0400

    connected to MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+//import mongoDB uri
+const database = require("./config/keys").mongoURI;
+
+//connect to database
+mongoose
+  .connect(database)
+  .then(() => console.log("connected to MongoDB"))
+  .catch(err => console.log("db connection error"));
 
 app.get("/", (req, res) => res.send("hello"));
 
