380fe6bf880845f9e2e893edd7096c321e7dcedd
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-22 19:55:36 -0400

    user model creation with schema

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Schema creation
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongood.model("users", UserSchema);
