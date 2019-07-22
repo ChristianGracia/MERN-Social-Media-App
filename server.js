003f566b87cdfd5d4b8570556f6fc3511bd2b562
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-22 19:17:33 -0400

    setting up routes for user authentication, profiles, and posts

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+//routes
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
 //import mongoDB uri
 const database = require("./config/keys").mongoURI;
 
@@ -12,6 +17,11 @@ mongoose
   .catch(err => console.log("db connection error"));
 
 app.get("/", (req, res) => res.send("hello"));
+
+//Routes
+app.use("/api/users", users);
+app.use("/api/users", posts);
+app.use("/api/users", profile);
 
 const port = process.env.PORT || 5000;
 
