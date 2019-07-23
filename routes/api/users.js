8388b4b317433d075943e73fb9fa0c14cbd5d535
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-22 21:23:34 -0400

    jwt token creation working and being sent

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
 
 // load user model
 const User = require("../../models/User");
@@ -55,7 +57,13 @@ router.post("/login", (req, res) => {
     // validate password
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        res.json({ msg: "Success" });
+        // create payload
+        const payload = { id: user.id, name: user.name, imgUrl: user.imgUrl };
+
+        // sign token
+        jwt.sign(payload, keys.secretKey, { expiresIn: 1800 }, (err, token) => {
+          res.json({ sucess: true, token: "Bearer " + token });
+        });
       } else {
         return res.status(400).json({ password: "Incorrect Password" });
       }
