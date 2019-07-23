3dc01f8b2b169f76389f69d1d4583a19f1749945
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-23 18:22:45 -0400

    validation complete

--- validation/register.js
+++ validation/register.js
@@ -5,6 +5,9 @@ module.exports = function validateRegisterInput(data) {
   let errors = {};
 
   data.name = !isEmpty(data.name) ? data.name : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
 
   if (!Validator.isLength(data.name, { min: 8, max: 16 })) {
     errors.name = "Name must be between 8 and 16 characters";
@@ -12,6 +15,28 @@ module.exports = function validateRegisterInput(data) {
   if (Validator.isEmpty(data.name)) {
     errors.name = "Name field is required";
   }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email is required";
+  }
+
+  if (Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+  if (Validator.isLength(data.password, { min: 6, max: 20 })) {
+    errors.password = "Password must be between 6 and 20 characters";
+  }
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm password field is required";
+  }
+  if (Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Passwords must match";
+  }
+
   return {
     errors,
     isValid: isEmpty(errors)
