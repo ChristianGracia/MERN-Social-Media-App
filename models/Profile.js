7101543bbe9fc5416784746f130700f0a005228a
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-23 23:43:29 -0400

    added work experience and education fields to profile schemma

--- models/Profile.js
+++ models/Profile.js
@@ -22,6 +22,72 @@ const ProfileSchema = new Schema({
     type: String
   },
   status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
     type: String
-  }
+  },
+  githubusername: {
+    type: String
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      }
+    }
+  ]
 });
