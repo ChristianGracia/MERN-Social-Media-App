1446f90bfb68e2ef6a13c88ba872974c969d7c33
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-23 23:55:35 -0400

    started setting up current user profile private route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
 
-//@access: public
+// import profile model
+const Profile = require("../../models/Profile");
+
+// import user profile
+const User = require("../../models/User");
+
+// @access: public
 router.get("/test", (req, res) => res.json({ msg: "Profile.js working" }));
 
+// Get current user profile
+// @access: private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id });
+  }
+);
 module.exports = router;
