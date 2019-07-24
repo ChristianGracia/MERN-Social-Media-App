f65affa6efe5a76c7b0b4d4bd745936f0d8530c4
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-23 23:35:06 -0400

    setting up Profile Schema

---
+++ models/Profile.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// create schema
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String
+  }
+});
