e26be288f1ed730434365b932e86a61b218384b7
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-23 23:58:24 -0400

    profile route set up

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -18,7 +18,16 @@ router.get(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    Profile.findOne({ user: req.user.id });
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
   }
 );
 module.exports = router;
