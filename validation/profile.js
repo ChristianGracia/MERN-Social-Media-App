d3b490429a5716acb5ec43e3431993d295121d9b
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-25 19:01:32 -0400

    adding profile validation

---
+++ validation/profile.js
@@ -0,0 +1,26 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateProfileInput(data) {
+  let errors = {};
+
+  data.handle = !isEmpty(data.email) ? data.handle : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email is required";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
