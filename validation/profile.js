c250d47f4095f7f99d72f0f6103002389172b160
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-25 19:05:03 -0400

    profile validation almost done

--- validation/profile.js
+++ validation/profile.js
@@ -8,6 +8,13 @@ module.exports = function validateProfileInput(data) {
   data.status = !isEmpty(data.status) ? data.status : "";
   data.skills = !isEmpty(data.skills) ? data.skills : "";
 
+  if (!Validator.isLength(data.handle, { min: 2, max: 30 })) {
+    errors.handle = "Handle needs to be between 2 and 30 characters";
+  }
+
+  if (!Validator.isEmpty(data.handle)) {
+    errors.handle = "Profile handle is required";
+  }
   return {
     errors,
     isValid: isEmpty(errors)
