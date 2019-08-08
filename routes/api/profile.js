8b922400a67afcbc2bb1eb8bbfaa57466ffa766a
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-07 22:47:40 -0400

    delete experience route working

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -226,4 +226,29 @@ router.post(
     });
   }
 );
+
+// DELTE api/profile/experience/:exp_id
+// delete experience from profile
+// @access private
+
+router.delete(
+  "/experience/:exp_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        // get removed experience index
+        const removeIndex = profile.experience
+          .map(item => item.id)
+          .indexOf(req.params.exp_id);
+
+        // take out of array
+        profile.experience.splice(removeIndex, 1);
+
+        // save new array
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 module.exports = router;
