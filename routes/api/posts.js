7269d2314e4dc2b34c1e84999b9b304ab5ac0a92
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-07 23:33:44 -0400

    setting up post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
 
-//@access: public
-router.get("/test", (req, res) => res.json({ msg: "Post.js working" }));
+const Post = require("../../models/Post");
+
+// POST api/posts
+// create post
+// @access private
+
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      imgUrl: req.body.name,
+      user: req.user.id
+    });
+
+    newPost.save();
+  }
+);
 
 module.exports = router;
