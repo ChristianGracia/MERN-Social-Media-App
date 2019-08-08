a823d24e625ec327d03b693d7e657cc9c7d8c93c
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-07 20:24:00 -0400

    added get profile by user id route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -50,6 +50,24 @@ router.get("/handle/:handle", (req, res) => {
     .catch((err = res.status(404).json(err)));
 });
 
+// GET api/profile/user/:user_id
+// Get profile by user ID
+// @access public
+
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
+  Profile.findOne({ handle: req.params.user_id })
+    .populate("user", ["name", "imgUrl"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch((err = res.status(404).json(err)));
+});
+
 // create or edit user profile
 // @access private
 
