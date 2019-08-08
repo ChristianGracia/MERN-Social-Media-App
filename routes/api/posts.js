49708092c7c505bae22bf91019225cfbb8d7af44
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-07 23:41:35 -0400

    linked validation to route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+// get Post model
 const Post = require("../../models/Post");
+
+// Validation
+const ValidatePostInput = require("../../validation/post");
 
 // POST api/posts
 // create post
@@ -13,6 +17,11 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = ValidatePostInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
     const newPost = new Post({
       text: req.body.text,
       name: req.body.name,
