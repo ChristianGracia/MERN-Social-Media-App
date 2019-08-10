7d1d4d9763b1a10e65eb6c58e80deaa86f008960
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-10 19:49:37 -0400

    added login and register route

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 class App extends Component {
   render() {
@@ -11,7 +13,11 @@ class App extends Component {
       <Router>
         <div className="App">
           <Navbar />
-          <Landing />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
           <Footer />
         </div>
       </Router>
