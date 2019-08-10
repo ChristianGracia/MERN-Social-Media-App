b87ca62687d3bd6c7e6400138bf642aa66ebdd83
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-10 19:09:06 -0400

    navbar working

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <h1>Hi</h1>
       </div>
     );
