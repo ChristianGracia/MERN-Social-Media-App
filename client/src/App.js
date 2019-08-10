1c5e78ac84bd1a9decf082ae48f0a1fe31c214c7
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-10 19:32:32 -0400

    landing set up

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <h1>Hi</h1>
+        <Landing />
         <Footer />
       </div>
     );
