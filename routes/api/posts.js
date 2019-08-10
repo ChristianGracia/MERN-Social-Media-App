dae442cfb467fbe253d5b889bbccb84623b22ec7
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-10 17:13:14 -0400

    unliking posts route fully functional

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -114,4 +114,40 @@ router.post(
   }
 );
 
+// POST api/posts/unlike/:id
+// unlike posts
+// @access private
+
+router.post(
+  "/unlike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length === 0
+          ) {
+            return res
+              .status(400)
+              .json({ notliked: "You have not yet liked this post" });
+          }
+
+          // get index on item to remove from like array
+          const removeIndex = post.likes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id);
+
+          // remove from array
+          post.likes.splice(removeIndex, 1);
+
+          // save
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+    });
+  }
+);
+
 module.exports = router;
