a5bdd62ee5e2089b31c4ca568c65efbeba9103c9
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-10 16:35:15 -0400

    added like post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -85,4 +85,33 @@ router.delete(
   }
 );
 
+// POST api/posts/like/:id
+// like posts
+// @access private
+
+router.post(
+  "like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyliked: "User already liked this post" });
+          }
+
+          // add user id to likes array
+          post.likes.unshift({ user: req.user.id });
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+    });
+  }
+);
+
 module.exports = router;
