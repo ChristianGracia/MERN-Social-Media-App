312baf6b048469baac9de22cb5a1e1c5051fb651
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-10 19:53:13 -0400

    navbar linked and working

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Navbar extends Component {
   render() {
@@ -29,14 +30,14 @@ class Navbar extends Component {
 
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <a className="nav-link" href="register.html">
-                  Sign Up
-                </a>
+                <Link className="nav-link" to="/register">
+                  Register
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="login.html">
+                <Link className="nav-link" to="/login">
                   Login
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
