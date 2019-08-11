c6170e97b8b79d7b0b5bb05de870f411ac020487
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-11 16:18:58 -0400

    setting up components for forms

---
+++ client/src/components/common/TextFieldGroup.js
@@ -0,0 +1,50 @@
+import React from "react";
+import classnames from "classnames";
+import propTypes from "prop-types";
+
+const TextFieldGroup = ({
+  name,
+  placeholder,
+  value,
+  label,
+  error,
+  info,
+  type,
+  onchange,
+  disabled
+}) => {
+  return (
+    <div className="form-group">
+      <input
+        type={type}
+        className={classnames("form-control form-control-lg", {
+          "is-invalid": error
+        })}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+      />
+      {info && <small className="form-text text-muted">{info}</small>}
+      {error && <div className="invalid-feedback">{error}</div>}
+    </div>
+  );
+};
+
+TextFieldGroup.propTypes = {
+  name: propTypes.string.isRequired,
+  placeholder: propTypes.string,
+  value: propTypes.string.isRequired,
+  info: propTypes.string,
+  error: propTypes.string,
+  type: propTypes.string.isRequired,
+  onChange: propTypes.func.isRequired,
+  disabled: propTypes.string
+};
+
+TextFieldGroup.defaultProps = {
+  type: "text"
+};
+
+export default TextFieldGroup;
