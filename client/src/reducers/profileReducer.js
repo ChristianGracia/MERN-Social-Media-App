67e40365ce43f79dda15fb8d4d8fa61775dc0350
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-11 17:18:50 -0400

    setting up profile reducer

---
+++ client/src/reducers/profileReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  profile: null,
+  profiles: null,
+  loading: false
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
