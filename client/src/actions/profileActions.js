7d1c20ae3ea65833d56511f8764cacb90a4823d4
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-11 17:32:32 -0400

    reducers set up for get current profile and profile is loading

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,8 +1,29 @@
 import axios from "axios";
 
-import { GET_PROFILE, PROFILE_LOADING, GET_ERROS } from "../types";
+import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS } from "../types";
 
 // get current profile
 export const getCurrentProfile = () => dispatch => {
   dispatch(setProfileLoading);
+  axios
+    .get("./api/profile")
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: {}
+      })
+    );
+};
+
+// profile loading
+export const setProfileLoading = () => {
+  return {
+    type: PROFILE_LOADING
+  };
 };
--- client/src/reducers/profileReducer.js
+++ client/src/reducers/profileReducer.js
@@ -1,3 +1,5 @@
+import { GET_PROFILE, PROFILE_LOADING } from "../actions/types";
+
 const initialState = {
   profile: null,
   profiles: null,
@@ -6,6 +8,17 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case PROFILE_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case GET_PROFILE:
+      return {
+        ...state,
+        profile: action.payload,
+        loading: false
+      };
     default:
       return state;
   }
