f02a012b1426f8d0c8d6a5f5d9b4945f30c2bd64
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-14 19:32:32 -0400

    all external links working even if a user doesnt put http:// in front of their social media link with template literals

--- client/src/components/profile/ProfileHeader.js
+++ client/src/components/profile/ProfileHeader.js
@@ -56,15 +56,37 @@ class ProfileHeader extends Component {
                     </a>
                   )}
 
-                  <a className="text-white p-2" href="#">
-                    <i className="fab fa-facebook fa-2x" />
-                  </a>
-                  <a className="text-white p-2" href="#">
-                    <i className="fab fa-linkedin fa-2x" />
-                  </a>
-                  <a className="text-white p-2" href="#">
-                    <i className="fab fa-instagram fa-2x" />
-                  </a>
+                  {isEmpty(profile.social && profile.social.facebook) ? null : (
+                    <a
+                      className="text-white p-2"
+                      href={`//${profile.social.facebook}`}
+                      target="_blank"
+                    >
+                      <i className="fab fa-facebook fa-2x" />
+                    </a>
+                  )}
+
+                  {isEmpty(profile.social && profile.social.linkedin) ? null : (
+                    <a
+                      className="text-white p-2"
+                      href={`//${profile.social.linkedin}`}
+                      target="_blank"
+                    >
+                      <i className="fab fa-linkedin fa-2x" />
+                    </a>
+                  )}
+
+                  {isEmpty(
+                    profile.social && profile.social.instagram
+                  ) ? null : (
+                    <a
+                      className="text-white p-2"
+                      href={`//${profile.social.instagram}`}
+                      target="_blank"
+                    >
+                      <i className="fab fa-instagram fa-2x" />
+                    </a>
+                  )}
                 </p>
               </div>
             </div>
