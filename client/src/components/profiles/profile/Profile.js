ea436afeff2475265de77e8e5f23130b386e42c6
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-14 16:58:18 -0400

    setting up profile by handle page

---
+++ client/src/components/profiles/profile/Profile.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Profilee</h1>
+      </div>
+    );
+  }
+}
+export default Profile;
