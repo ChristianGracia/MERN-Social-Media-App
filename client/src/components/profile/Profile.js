c589b2fcf6963d69dbba9db7cdeb3d4cd9563d20
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-14 19:44:21 -0400

    skills now being displayed on profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -34,7 +34,7 @@ class Profile extends Component {
             <div className="col-md-6" />
           </div>
           <ProfileHeader profile={profile} />
-          <ProfileAbout />
+          <ProfileAbout profile={profile} />
           <ProfileCreds />
           <ProfileGithub />
         </div>
--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,12 +1,40 @@
 import React, { Component } from "react";
+import Profile from "./Profile";
+import PropTypes from "prop-types";
+import isEmpty from "../../validation/is-empty";
 
 class ProfileAbout extends Component {
   render() {
+    const { profile } = this.props;
+
+    const firstName = profile.user.name.trim().split(" ")[0];
+
+    const skills = profile.skills.map((skill, index) => (
+      <div key={index} className="p-3">
+        <i className="fa fa-check" />
+        {skill}
+      </div>
+    ));
     return (
-      <div>
-        <h1>Profile About</h1>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card card-body bg-light mb-3">
+            <h3 class="text-center text-info">{firstName}</h3>
+            <p class="lead">
+              {isEmpty(profile.bio) ? null : <span>{profile.bio}</span>}
+            </p>
+            <hr />
+            <h3 class="text-center text-info">Skill Set</h3>
+            <div class="row">
+              <div class="d-flex flex-wrap justify-content-center align-items-center">
+                {skills}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
 export default ProfileAbout;
