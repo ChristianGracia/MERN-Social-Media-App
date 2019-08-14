9f2961564a4600aea8baa5a1b696f3f780b6ecdf
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-14 17:14:09 -0400

    added get profile by handle action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -155,3 +155,23 @@ export const getProfiles = () => dispatch => {
       })
     );
 };
+
+// get profile by handle
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get("/api/profile/all")
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
+      })
+    );
+};
+
--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
+import { getProfileByHandle } from "../../actions/profileActions";
+import Spinner from "../common/Spinner";
 import ProfileHeader from "./ProfileHeader";
 import ProfileCreds from "./ProfileCreds";
 import ProfileGithub from "./ProfileGithub";
 import ProfileAbout from "./ProfileAbout";
 
 class Profile extends Component {
+  componentDidMount() {
+    if (this.props.match.params.handle) {
+      this.props.getProfileByHandle(this.props.match.params.handle);
+    }
+  }
   render() {
     return (
       <div>
@@ -14,4 +24,12 @@ class Profile extends Component {
     );
   }
 }
-export default Profile;
+
+Profile.propTypes = {
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => {
+  profile: state.profile;
+};
+export default connect(mapStateToProps)(Profile);
