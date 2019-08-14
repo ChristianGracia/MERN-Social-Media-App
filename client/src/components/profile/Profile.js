5df78b5a03c7afced5909492fa3d02f56ea4504e
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-14 17:03:27 -0400

    clean

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import ProfileHeader from "./ProfileHeader";
+import ProfileCreds from "./ProfileCreds";
+import ProfileGithub from "./ProfileGithub";
+import ProfileAbout from "./ProfileAbout";
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Profile</h1>
+      </div>
+    );
+  }
+}
+export default Profile;
