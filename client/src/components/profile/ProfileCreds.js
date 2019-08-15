3c6bebc6d733d25bc83352bca90db6dbc2001e15
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-14 20:14:24 -0400

    education being posted on profile too

--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -34,9 +34,60 @@ class ProfileCreds extends Component {
         </p>
       </li>
     ));
+
+    const eduItems = education.map(edu => (
+      <li key={edu._id} className="list-group-item">
+        <h4>{edu.school}</h4>
+        <p>
+          <Moment format="YYYY/MM/DD">{edu.from}</Moment> -
+          {edu.to == null ? (
+            " Now"
+          ) : (
+            <Moment format="YYYY/MM/DD">{edu.from}</Moment>
+          )}
+        </p>
+        <p>
+          <strong>Degree:</strong> {edu.degree}
+        </p>
+        <p>
+          <strong>Field of study:</strong> {edu.fieldofstudy}
+        </p>
+        <p>
+          {edu.location === "" ? null : (
+            <span>
+              <strong>Location: </strong> {edu.location}
+            </span>
+          )}
+        </p>
+        <p>
+          {edu.description === "" ? null : (
+            <span>
+              <strong>Description: </strong> {edu.description}
+            </span>
+          )}
+        </p>
+      </li>
+    ));
+
     return (
-      <div>
-        <h1>Profile Creds</h1>
+      <div className="row">
+        <div className="col-md-6">
+          <h3 className="text-center text-info">Experience</h3>
+          {expItems.length > 0 ? (
+            <ul className="list-group">{expItems}</ul>
+          ) : (
+            <p className="text-center">No experience added</p>
+          )}
+        </div>
+
+        <div className="col-md-6">
+          <h3 className="text-center text-info">Education</h3>
+          {eduItems.length > 0 ? (
+            <ul className="list-group">{eduItems}</ul>
+          ) : (
+            <p className="text-center">No Education Listed</p>
+          )}
+        </div>
       </div>
     );
   }
