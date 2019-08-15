1813428f672d200222d4ed5a0d0472a3ccd60707
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-14 22:47:10 -0400

    create post complete, now setting up the get all posts profile action

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ADD_POST, GET_ERRORS } from "./types";
+import { ADD_POST, GET_ERRORS, GET_Posts } from "./types";
 import { bindActionCreators } from "redux";
 
 //add post
@@ -18,4 +18,22 @@ export const addPost = postData => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+//get post
+export const addPost = postData => dispatch => {
+    axios
+        .post("api/posts", postData)
+        .then(res =>
+            dispatch({
+                type: ADD_POST,
+                payload: res.data
+            })
+        )
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
 };
