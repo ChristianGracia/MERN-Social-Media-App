14e828678e76b874c6865a20a4ab069cf4dbadf3
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-14 21:53:31 -0400

    post action set up

---
+++ client/src/actions/postActions.js
@@ -0,0 +1,2 @@
+import axios from "axios";
+import { ADD_POST, GET_ERRORS } from "./types";
--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,3 +1,11 @@
+// import {
+//   ADD_POST,
+//   GET_POSTS,
+//   GET_POST,
+//   DELETE_POST,
+//   POST_LOADING
+// } from "../actions/types";
+
 const initialState = {
   posts: [],
   post: [],
