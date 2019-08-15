a2528434dbda7dd9d97c88fc88c4a0061cd8b36a
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-15 01:01:06 -0400

    comment page now rendering post info with like and delete button omitted with conditonal rendering

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -3,15 +3,38 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import Spinner from "../common/Spinner";
 import { getPost } from "../../actions/postActions";
+import PostItem from "../posts/PostItem";
+import { Link } from "react-router-dom";
 
 class Post extends Component {
   componentDidMount() {
     this.props.getPost(this.props.match.params.id);
   }
   render() {
+    const { post, loading } = this.props.post;
+    let postContent;
+
+    if (post == null || loading || Object.keys(post).length === 0) {
+      postContent = <Spinner />;
+    } else {
+      postContent = (
+        <div>
+          <PostItem post={post} showActions={false} />
+        </div>
+      );
+    }
     return (
-      <div>
-        <h1>hi</h1>
+      <div class="post">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <Link to="/feed" className="btn btn-light mb-3">
+                Back To Feed
+              </Link>
+              {postContent}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
