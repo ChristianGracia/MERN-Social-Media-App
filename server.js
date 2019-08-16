2f6d34d649cabae6a72bed97856ce537e6ae42ee
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-16 18:32:59 -0400

    ready to deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 // routes
 const users = require("./routes/api/users");
@@ -30,10 +31,18 @@ app.use(passport.initialize());
 // passport config
 require("./config/passport")(passport);
 
-//Routes
+// routes
 app.use("/api/users", users);
 app.use("/api/posts", posts);
 app.use("/api/profile", profile);
+
+// set server static asses if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
